64b1dd01aa2b70d1a46f216e2b13f648247f2d5f
simplo78 <79467453+simplo78@users.noreply.github.com>
2023-05-01 10:46:19 -0400

    Update Jenkins-redhat8-install28.sh

--- JENKINS-INSTALLATION/Jenkins-redhat8-install28.sh
+++ JENKINS-INSTALLATION/Jenkins-redhat8-install28.sh
@@ -8,7 +8,7 @@ sudo hostnamectl set-hostname jenkins
 sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
-sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
 sudo yum upgrade -y
 # Add required dependencies for the jenkins package
 sudo yum install java-11-openjdk -y
